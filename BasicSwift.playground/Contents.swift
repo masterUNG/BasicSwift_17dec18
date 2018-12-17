7f349fc14908e51d65c77c3258b90008719ec5e3
masterUNG <phrombutr@gmail.com>
2018-12-17 10:37:28 +0700

    Show Console

--- BasicSwift.playground/Contents.swift
+++ BasicSwift.playground/Contents.swift
@@ -9,6 +9,22 @@ nameString = "Doramon"
 let name2String = "Nopita"
 //name2String = "TestNopita"    ไม่สามารถ Compile ได้ เพราะ let ไม่สามารถแก้ไขได้แบบ var
 
+//Explicit คือ การประกาศตัวแปร แบบ กำหนด Data Type
+var name: String = "Doramon"
+var number1: Int = 123
+var number2: Double = 12.34
+var status: Bool = true
+
+//Show Console
+print("This is Swift")
+print("nameString ==> " + nameString)   // Java Type
+print("nameString ==> \(nameString)")   // Swift Type
+print("nubmer1 ==> \(number1)")
+
+
+
+
+
 
 
 
