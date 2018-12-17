5a6ee7d682cd813de42bc956979ffe494de9ab0d
masterUNG <phrombutr@gmail.com>
2018-12-17 10:21:50 +0700

    Variable and Constant

---
+++ BasicSwift.playground/Contents.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+//Implicit Variable and Constant
+var nameString = "Master UNG"
+var numberInt = 123
+
+nameString = "Doramon"
+
+let name2String = "Nopita"
+//name2String = "TestNopita"    ไม่สามารถ Compile ได้ เพราะ let ไม่สามารถแก้ไขได้แบบ var
+
+
+
+
